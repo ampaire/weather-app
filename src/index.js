9dd6c1947736844e07a6e68f1caf5373fa26e771
ampaire <femayapr@gmail.com>
2020-04-03 16:14:33 -0600

    Refactor code

--- src/index.js
+++ src/index.js
@@ -7,23 +7,20 @@ import parseJSON from './modules/parsejson';
 const apiKey = '034b1e6439b4af8304cfb5a8734b6e14';
 searchForm.addEventListener('submit', event => {
   event.preventDefault();
-
-
-  let data = {};
   const currentCity = checkCity.value;
-  const metricURL = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=metric`;
-  const fehrenhitURL = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=imperial`;
-  fetch(metricURL, { mode: 'cors' })
-    .then(response => response.json())
-    .then(parseJSON)
-    .then(dataMetric => { data = dataMetric; })
-    .then(() => fetch(fehrenhitURL))
-    .then(response => response.json())
-    .then(parseJSON)
-    .then(({ temp }) => updateDOM(data, temp))
-    .catch(() => {
-      errorMessage.textContent = 'I Couldn\'t fetch weather for this city 😩.';
-    });
+  const fetchWeather = async (city, unit) => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`,
+    );
+    return response.json();
+  };
+
+  Promise.all([
+    fetchWeather(currentCity, 'metric'),
+    fetchWeather(currentCity, 'imperial'),
+  ]).then(([celsiusData, FahrenheitData]) => {
+    updateDOM(parseJSON(celsiusData), parseJSON(FahrenheitData).tempF);
+  });
   errorMessage.textContent = '';
   searchForm.reset();
   checkCity.focus();
